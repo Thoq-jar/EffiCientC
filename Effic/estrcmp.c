420f01311dc72b413915c74b850c6cc65cf5574a
Thoq-jar <tristanshaw000@gmail.com>
2024-10-13 01:41:23 -0400

    fixed vulnerablity in estrcmp :sob:

--- Effic/estrcmp.c
+++ Effic/estrcmp.c
@@ -18,12 +18,12 @@
 #include "effic.h"
 
 int estrcmp(const char *s1, const char *s2) {
-  while (*s1 != '\0' || *s2 != '\0') {
+  while (*s1 != '\0' && *s2 != '\0') {
     if (*s1 != *s2) {
       return *s1 - *s2;
     }
     s1++;
     s2++;
   }
-  return 0;
+  return *s1 - *s2;
 }
